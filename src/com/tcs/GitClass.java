d4d15176d7a309121507d42c247154f77d98e524
kuttimalathi <kuttimalathi685@gmail.com>
2023-05-03 23:40:39 +0530

    new update

--- src/com/tcs/GitClass.java
+++ src/com/tcs/GitClass.java
@@ -3,6 +3,6 @@ package com.tcs;
 public class GitClass {
    public static void main(String[] args) {
       System.out.println("git message");
-      System.out.println("Malathi Updated code");
+      System.out.println("Malathi new code");
    }
 }
